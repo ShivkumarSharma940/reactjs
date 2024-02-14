a80b1f83bc8f5199bce76eda429c00962fa2a86d
Amit <shivkumarsharma940@gmail.com>
2024-02-14 11:00:11 +0530

    deleting previous files

--- 01girish/src/App.js
+++ 01girish/src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+function MyGreetings (){
+  return(
+    <>
+     <h1> Jay shri ram jay shri hari vishnu jai ma laxmi maa</h1>
+    </>
+  )
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   <>
+    <div>
+      <h1> This starting with a react project </h1>
+      <MyGreetings></MyGreetings>
     </div>
+   </>
   );
 }
+
+ 
+
 
 export default App;
